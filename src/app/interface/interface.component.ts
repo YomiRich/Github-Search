b3933c4c6ea4442edbe62860eaf0fac96073a502
Yomi Rich <ymueni@gmail.com>
2019-07-15 12:45:42 +0300

    Add content to the UI and added functionality

--- src/app/interface/interface.component.ts
+++ src/app/interface/interface.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, OnInit, ComponentFactory } from '@angular/core';
+import { GitServe } from '../git-serv.service';
+import { User } from '../user';
+import { Repos } from '../repositories';
+
 
 @Component({
-  selector: 'app-interface',
-  templateUrl: './interface.component.html',
-  styleUrls: ['./interface.component.css']
+  selector: 'app-inteface',
+  templateUrl: './inteface.component.html',
+  styleUrls: ['./inteface.component.css']
 })
 export class InterfaceComponent implements OnInit {
+  username:string;
+  profile:User;
+  repos:Repos
+  constructor(private profileService: GitServe) {
 
-  constructor() { }
+  }
+
+  findProfile(){
+  	this.profileService.updateProfile(this.username)
+  	this.profileService.getProfileInfo().subscribe(profile => {
+  		console.log(profile);
+  		this.profile = profile;
+  	})
+
+  	this.profileService.getProfileRepos().subscribe(repos => {
+  		console.log(repos);
+  		this.repos = repos;
+  	})
+  }
 
   ngOnInit() {
+    interface ApiResponse{
+    name: string;
+    login: string;
+    avatar_url: any;
+    html_url: string;
+    public_repos: string;
+    }
   }
 
 }
